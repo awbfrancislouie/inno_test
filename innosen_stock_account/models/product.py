5eb43a991cd70d75f70952acf478ba8653656450
awbfrancis <francis.delrosario@achievewithoutborders.com>
2021-08-03 15:40:02 +0800

    [FIX]

--- innosen_stock_account/models/product.py
+++ innosen_stock_account/models/product.py
@@ -1,5 +1,55 @@
 from odoo.exceptions import UserError
+from odoo.addons.stock_account.models.product import ProductTemplate as OriginalProductTemplate
 from odoo.addons.stock_account.models.product import ProductProduct as OriginalProductProduct
+
+def write(self, vals):
+    impacted_templates = {}
+    move_vals_list = []
+    Product = self.env['product.product']
+    SVL = self.env['stock.valuation.layer']
+
+    if 'categ_id' in vals:
+        # When a change of category implies a change of cost method, we empty out and replenish
+        # the stock.
+        new_product_category = self.env['product.category'].browse(vals.get('categ_id'))
+
+        for product_template in self:
+            valuation_impacted = False
+            if product_template.cost_method != new_product_category.property_cost_method:
+                valuation_impacted = True
+            if product_template.valuation != new_product_category.property_valuation:
+                valuation_impacted = True
+            if valuation_impacted is False:
+                continue
+
+            # Empty out the stock with the current cost method.
+            description = _("Due to a change of product category (from %s to %s), the costing method\
+                            has changed for product template %s: from %s to %s.") %\
+                (product_template.categ_id.display_name, new_product_category.display_name, \
+                 product_template.display_name, product_template.cost_method, new_product_category.property_cost_method)
+            out_svl_vals_list, products_orig_quantity_svl, products = Product\
+                ._svl_empty_stock(description, product_template=product_template)
+            out_stock_valuation_layers = SVL.create(out_svl_vals_list)
+            if product_template.valuation == 'real_time':
+                move_vals_list += Product._svl_empty_stock_am(out_stock_valuation_layers)
+            impacted_templates[product_template] = (products, description, products_orig_quantity_svl)
+
+    res = super(ProductTemplate, self).write(vals)
+
+    for product_template, (products, description, products_orig_quantity_svl) in impacted_templates.items():
+        # Replenish the stock with the new cost method.
+        in_svl_vals_list = products._svl_replenish_stock(description, products_orig_quantity_svl)
+        in_stock_valuation_layers = SVL.create(in_svl_vals_list)
+        if product_template.valuation == 'real_time':
+            move_vals_list += Product._svl_replenish_stock_am(in_stock_valuation_layers)
+
+    # Create the account moves.
+    if move_vals_list:
+        account_moves = self.env['account.move'].create(move_vals_list)
+    return res
+
+OriginalProductTemplate.write = write
+
 
 def _change_standard_price(self, new_price):
     """Helper to create the stock valuation layers and the account moves
