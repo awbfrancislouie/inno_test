41da15aa63d930a6a574f55df99738056e8c4052
awbfrancis <francis.delrosario@achievewithoutborders.com>
2021-08-03 14:56:28 +0800

    [ADD] - custom module

---
+++ innosen_stock_account/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   ACHIEVE WITHOUT BORDERS
+#
+##############################################################################
+{
+    'name': "Innosen Stock Account",
+    'summary': """
+        Innosen Stock Account
+        """,
+    'description': """
+        Innosen Stock Account
+    """,
+    'author': "Achieve Without Borders, Inc",
+    'website': "http://www.achievewithoutborders.com",
+    'category': 'Accounting',
+    'version': '13.0.1.0.0',
+    'depends': ['stock_account'],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
